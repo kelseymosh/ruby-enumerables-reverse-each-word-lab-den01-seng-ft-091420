0df666787d03341d79290554b39146c8099e34a8
Kelsey Oshiro <kelseymosh@gmail.com>
2020-08-13 19:52:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence = "Hello there, and how are you?"
-  reverse = sentence.split("").collect do |words|
+  reverse = sentence.split(" ").collect do |words|
     words.reverse!
   end
   reverse.join("")
